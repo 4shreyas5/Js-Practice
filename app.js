06cabed6172e0c8d74ec921fbf093e4f589266b3
Shreyas <shreyassingh621@gmail.com>
2023-12-23 11:35:43 +0530

    Updation 2

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-let favMovie = 'Theri'
+// let favMovie = 'Theri'
 
-let guess  = prompt("Guess my Favourite Movie: ")
+// let guess  = prompt("Guess my Favourite Movie: ")
 
-while ((guess != favMovie) && (guess != 'quit')){
-    guess = prompt("Wrong Guess. Please try again! ")
-}
-if (guess == favMovie){
-    console.log("You Won")
-}
-else{
-    console.log("You Quit")
+// while ((guess != favMovie) && (guess != 'quit')){
+//     guess = prompt("Wrong Guess. Please try again! ")
+// }
+// if (guess == favMovie){
+//     console.log("You Won")
+// }
+// else{
+//     console.log("You Quit")
+// }
+
+//Object Literals
+
+// let student = {
+//     name : "Shreyas",
+//     rollnu : 3464,
+//     marks : 99,
+//     post : "President"
+// }
+
+//Nested Objects
+const classInfo = {
+    aman : {
+        grade: "A+",
+        marks: 95
+    },
+    atul : {
+        grade: "A",
+        marks: 90,
+    },
+    ahan : {
+        grade: "B",
+        marks: 80
+    }
 }
