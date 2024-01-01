887e1db7b019a910bf22afb39772516fd0f04793
Shreyas <shreyassingh621@gmail.com>
2024-01-02 03:20:36 +0530

    TRy Catch

---
+++ trycatch.js
@@ -0,0 +1,12 @@
+try {
+    console.log("Hello")
+    console.log("Hello")
+    console.log("Hello")
+    console.log(a)
+    console.log("Hello2")
+    console.log("Hello2")
+} catch (error) {
+    console.log(error)
+}
+console.log("Hello2")
+console.log("Hello2")
