1629990f8468b7cb9042af8e1d2e15b4433ca814
Shreyas <shreyassingh621@gmail.com>
2024-01-02 03:17:13 +0530

    this keyword

---
+++ this.js
@@ -0,0 +1,12 @@
+const student = {
+
+    name : "Shreyas",
+    roll : 64,
+    eng : 99,
+    math : 100,
+    getAvg(){
+        let avg = (this.eng + this.math)/2;
+        console.log(avg);
+    }
+
+}
