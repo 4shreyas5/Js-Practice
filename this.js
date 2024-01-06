51a7585c6d650129966ade7aeaa28686bc94a1a2
Shreyas <shreyassingh621@gmail.com>
2024-01-06 19:01:10 +0530

    This Practice

--- this.js
+++ this.js
@@ -1,9 +1,22 @@
-const student = {
+const student1 = {
 
     name : "Shreyas",
     roll : 64,
     eng : 99,
     math : 100,
+    getAvg(){
+        let avg = (this.eng + this.math)/2;
+        console.log(avg);
+    }
+
+}
+
+const student2 = {
+
+    name : "Jyotendra",
+    roll : 32,
+    eng : 100,
+    math : 99,
     getAvg(){
         let avg = (this.eng + this.math)/2;
         console.log(avg);
