cb15bf83c08401cb105cfeb2d3772b21dc846db1
Shreyas <shreyassingh621@gmail.com>
2024-01-13 22:15:49 +0530

    Rest and Spread

--- part8.js
+++ part8.js
@@ -1,4 +1,4 @@
-let arr = [1,2,3,4,5,6,7,,8,9]
+let arr = [1,2,3,4,5,6,7,8,9]
 
 // function print(el) {
 //     console.log(el)
@@ -27,3 +27,16 @@ let array = [
 ]
 
 let gpa = array.map((el) => el.marks/10)
+ //
+
+ let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+ let even  =  a.filter((el) => (el%2 == 0))
+// spread
+
+let obj = {...arr}
+//rest
+
+function mini (a,b,c,d){
+    console.log(arguments)
+}
