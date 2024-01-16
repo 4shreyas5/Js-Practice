93501e1870380690df1d7497da4f214a6f862754
Shreyas <shreyassingh621@gmail.com>
2024-01-16 20:48:32 +0530

    Assignment Part 1 Done

---
+++ Assignments/assignment1.js
@@ -0,0 +1,15 @@
+//1
+let num = prompt("NUM = ")
+ 
+if (num % 10 === 0){
+    console.log("good")
+}
+else(
+    console.log("bad")
+)
+
+//2
+let name1 = prompt("Please Enter Your Name");
+let age  = prompt("Please Enter Your Age");
+
+alert(`${name1} is ${age} Years Old`)
