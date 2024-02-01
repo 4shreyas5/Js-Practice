781222d0fcf229452ffbf3b74e0caead03cdac58
Shreyas <shreyassingh621@gmail.com>
2024-02-01 20:39:40 +0530

    Query Selectors

--- DOM/app.js
+++ DOM/app.js
@@ -1,4 +1,11 @@
-let a = document.getElementById("mainImg")
-a.src = "assets/creation_1.png"
+// let a = document.getElementById("mainImg")
+// a.src = "assets/creation_1.png"
 
-let b =  document.getElement
+// let b =  document.getElement
+
+//Query Selectors
+console.dir(document.querySelector('h1'))
+
+console.dir(document.querySelector('#description'))
+
+console.dir(document.querySelector('.oldImg'))
